ab7d9fab20bc2054332c5d0340563b65b12a13ae
Dave Cheney <dave@cheney.net>
2016-02-29 14:29:44 +1100

    Initial commit

---
+++ main.go
@@ -0,0 +1,80 @@
+// prdeps prints the dependency graph of a Go package.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"go/build"
+	"log"
+	"os"
+	"strings"
+	"text/template"
+)
+
+// cache of resolved packages
+var pkgcache = make(map[string]*build.Package)
+
+var stdlib bool
+var tmpl string
+
+func spaces(n int) string {
+	return strings.Repeat(" ", n*2)
+}
+
+func loadpkg(importpath string) *build.Package {
+	pkg, ok := pkgcache[importpath]
+	if ok {
+		return pkg
+	}
+
+	pkg, err := build.Import(importpath, "", 0)
+	if err != nil {
+		log.Fatalf("could not locate %q: %v", importpath, err)
+	}
+
+	pkgcache[pkg.ImportPath] = pkg
+	return pkg
+}
+
+func printpkg(importpath string, t *template.Template, depth int) {
+	switch importpath {
+	case "C", "unsafe":
+		// fake packages, ignore
+		return
+	}
+
+	pkg := loadpkg(importpath)
+	if pkg.Goroot && !stdlib {
+		// do not traverse into the stdlib unless requested
+		return
+	}
+
+	fmt.Print(spaces(depth))
+	t.Execute(os.Stdout, pkg)
+	fmt.Println()
+
+	depth++
+	for _, dep := range pkg.Imports {
+		printpkg(dep, t, depth)
+	}
+}
+
+func main() {
+	flag.BoolVar(&stdlib, "s", false, "include stdlib")
+	flag.StringVar(&tmpl, "f", "{{.ImportPath}}:", "output format")
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) < 1 {
+		flag.Usage()
+	}
+
+	t, err := template.New("").Parse(tmpl)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, pkg := range args {
+		printpkg(pkg, t, 0)
+	}
+}
